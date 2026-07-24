825cc1c8df3d5921ec9e62eca83fa8edb1febe2d
hebin123456 <hebin123456@users.noreply.github.com>
2026-07-24 01:49:17 +0000

    refactor(4.5-c): migrate BranchViewModel from WPF Media to Avalonia Media
    - Replace System.Windows.Media with Avalonia.Media
    - Use Color.Parse instead of ColorConverter.ConvertFromString
    - Remove Freeze() calls (Avalonia SolidColorBrush has no Freeze method)

--- src/ForkPlus/UI/BranchViewModel.cs
+++ src/ForkPlus/UI/BranchViewModel.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel;
-using System.Windows.Media;
+using Avalonia.Media;
 using ForkPlus.Settings;
 
 namespace ForkPlus.UI
@@ -70,88 +70,71 @@ namespace ForkPlus.UI
 			BackgroundBrushChangedEventArgs = new PropertyChangedEventArgs("BackgroundBrush");
 			_borderBrushesLight = new SolidColorBrush[13]
 			{
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF9502")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFCC00")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF3B30")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#A2845E")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#64DA38")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1CADF8")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CB73E1")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8E8E91")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF2968")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#30D5C8")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5856D6")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#B4D435")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF6F61"))
+				new SolidColorBrush(Color.Parse("#FF9502")),
+				new SolidColorBrush(Color.Parse("#FFCC00")),
+				new SolidColorBrush(Color.Parse("#FF3B30")),
+				new SolidColorBrush(Color.Parse("#A2845E")),
+				new SolidColorBrush(Color.Parse("#64DA38")),
+				new SolidColorBrush(Color.Parse("#1CADF8")),
+				new SolidColorBrush(Color.Parse("#CB73E1")),
+				new SolidColorBrush(Color.Parse("#8E8E91")),
+				new SolidColorBrush(Color.Parse("#FF2968")),
+				new SolidColorBrush(Color.Parse("#30D5C8")),
+				new SolidColorBrush(Color.Parse("#5856D6")),
+				new SolidColorBrush(Color.Parse("#B4D435")),
+				new SolidColorBrush(Color.Parse("#FF6F61"))
 			};
 			_borderBrushesDark = new SolidColorBrush[13]
 			{
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F28B1F")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#BF9A2F")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CB2327")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#A68357")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#27A649")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#0082BA")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#9D53A0")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6D6D6E")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CC1F56")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#20A89E")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4A48B0")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8DAA28")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E05A4D"))
+				new SolidColorBrush(Color.Parse("#F28B1F")),
+				new SolidColorBrush(Color.Parse("#BF9A2F")),
+				new SolidColorBrush(Color.Parse("#CB2327")),
+				new SolidColorBrush(Color.Parse("#A68357")),
+				new SolidColorBrush(Color.Parse("#27A649")),
+				new SolidColorBrush(Color.Parse("#0082BA")),
+				new SolidColorBrush(Color.Parse("#9D53A0")),
+				new SolidColorBrush(Color.Parse("#6D6D6E")),
+				new SolidColorBrush(Color.Parse("#CC1F56")),
+				new SolidColorBrush(Color.Parse("#20A89E")),
+				new SolidColorBrush(Color.Parse("#4A48B0")),
+				new SolidColorBrush(Color.Parse("#8DAA28")),
+				new SolidColorBrush(Color.Parse("#E05A4D"))
 			};
 			_backgroundBrushesLight = new SolidColorBrush[13]
 			{
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF2DD")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF9DC")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE5E4")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F3F0EA")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E9FBE4")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#DFF5FF")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FAECFC")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F1F1F1")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE3EC")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#DEF7F5")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#E5E5F7")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F2F9DE")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE5E2"))
+				new SolidColorBrush(Color.Parse("#FFF2DD")),
+				new SolidColorBrush(Color.Parse("#FFF9DC")),
+				new SolidColorBrush(Color.Parse("#FFE5E4")),
+				new SolidColorBrush(Color.Parse("#F3F0EA")),
+				new SolidColorBrush(Color.Parse("#E9FBE4")),
+				new SolidColorBrush(Color.Parse("#DFF5FF")),
+				new SolidColorBrush(Color.Parse("#FAECFC")),
+				new SolidColorBrush(Color.Parse("#F1F1F1")),
+				new SolidColorBrush(Color.Parse("#FFE3EC")),
+				new SolidColorBrush(Color.Parse("#DEF7F5")),
+				new SolidColorBrush(Color.Parse("#E5E5F7")),
+				new SolidColorBrush(Color.Parse("#F2F9DE")),
+				new SolidColorBrush(Color.Parse("#FFE5E2"))
 			};
 			_backgroundBrushesDark = new SolidColorBrush[13]
 			{
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5D3D14")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5B4C0E")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5F2425")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#433A32")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#285224")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#13445B")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#503455")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D3D3F")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#601E35")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1A4A45")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#272650")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D4E1A")),
-				new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5D2A24"))
+				new SolidColorBrush(Color.Parse("#5D3D14")),
+				new SolidColorBrush(Color.Parse("#5B4C0E")),
+				new SolidColorBrush(Color.Parse("#5F2425")),
+				new SolidColorBrush(Color.Parse("#433A32")),
+				new SolidColorBrush(Color.Parse("#285224")),
+				new SolidColorBrush(Color.Parse("#13445B")),
+				new SolidColorBrush(Color.Parse("#503455")),
+				new SolidColorBrush(Color.Parse("#3D3D3F")),
+				new SolidColorBrush(Color.Parse("#601E35")),
+				new SolidColorBrush(Color.Parse("#1A4A45")),
+				new SolidColorBrush(Color.Parse("#272650")),
+				new SolidColorBrush(Color.Parse("#3D4E1A")),
+				new SolidColorBrush(Color.Parse("#5D2A24"))
 			};
-			SolidColorBrush[] borderBrushesLight = _borderBrushesLight;
-			for (int i = 0; i < borderBrushesLight.Length; i++)
-			{
-				borderBrushesLight[i].Freeze();
-			}
-			borderBrushesLight = _borderBrushesDark;
-			for (int i = 0; i < borderBrushesLight.Length; i++)
-			{
-				borderBrushesLight[i].Freeze();
-			}
-			borderBrushesLight = _backgroundBrushesLight;
-			for (int i = 0; i < borderBrushesLight.Length; i++)
-			{
-				borderBrushesLight[i].Freeze();
-			}
-			borderBrushesLight = _backgroundBrushesDark;
-			for (int i = 0; i < borderBrushesLight.Length; i++)
-			{
-				borderBrushesLight[i].Freeze();
-			}
+			// Avalonia SolidColorBrush 没有 Freeze() 方法（WPF 概念）。
+			// 这些画刷创建后存储在 static readonly 字段中且不再修改，
+			// 因此无需冻结即可安全使用。
 		}
 
 		public BranchViewModel(int graphColumn)
